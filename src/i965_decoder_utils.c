00260be919d6ba377774bbd982e6768f12f2cb2c
Gwenole Beauchesne <gwenole.beauchesne@intel.com>
2014-04-23 17:23:21 +0200

    vp8: fix support for segmentation-enabled streams.
    If segmentation is enabled, then the segmentation map shall be live
    across frames until the current frame updates the segment ids. This
    means that the driver needs to maintain the segmentation map buffer
    allocation and enable writes (resp. reads) whenever necessary.
    
    This fixes decoding of 00-comprehensive-010.
    
    Signed-off-by: Gwenole Beauchesne <gwenole.beauchesne@intel.com>
    (cherry picked from commit 61fbb1bba1ad8a41ffae4fd1ba90391adf819b6e)

--- src/i965_decoder_utils.c
+++ src/i965_decoder_utils.c
@@ -786,3 +786,27 @@ intel_decoder_sanity_check_input(VADriverContextP ctx,
 out:
     return vaStatus;
 }
+
+/* Ensure the segmentation buffer is large enough for the supplied
+   number of MBs, or re-allocate it */
+bool
+intel_ensure_vp8_segmentation_buffer(VADriverContextP ctx, GenBuffer *buf,
+    unsigned int mb_width, unsigned int mb_height)
+{
+    struct i965_driver_data * const i965 = i965_driver_data(ctx);
+    /* The segmentation map is a 64-byte aligned linear buffer, with
+       each cache line holding only 8 bits for 4 continuous MBs */
+    const unsigned int buf_size = ((mb_width + 3) / 4) * 64 * mb_height;
+
+    if (buf->valid) {
+        if (buf->bo && buf->bo->size >= buf_size)
+            return true;
+        drm_intel_bo_unreference(buf->bo);
+        buf->valid = false;
+    }
+
+    buf->bo = drm_intel_bo_alloc(i965->intel.bufmgr, "segmentation map",
+        buf_size, 0x1000);
+    buf->valid = buf->bo != NULL;
+    return buf->valid;
+}
